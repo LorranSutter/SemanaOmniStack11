29cb4206ee5bf4259b23c2074b6b5a0d24bcbd50
Lorran Sutter <sutterlorran@gmail.com>
2020-03-24 20:38:27 -0400

    frontend and backend initial code

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Route / Resource
+
+/**
+ * HTTP methods:
+ * 
+ * GET: Search information in backend
+ * POST: Create information in backend
+ * PUT: Change information in backend
+ * DELETE: Delete information in backend
+ */
+
+
+/**
+ * Parameter types:
+ * 
+ * Query Params: Named params sent in the rout after "?" (Filters, pagination)
+ * Route Params: Used to identify resources
+ * Request body: Request body used to create or change resources
+ */
+
+ /**
+  * SQL: MySQL, SQLite, PostgreSQL, Oracle, MS SQL Server, etc...
+  * NoSQL: MongoDB, CouchDB, Firebase, etc...
+  */
+
+  /**
+   * Driver: SELECT * FROM users
+   * Query Builder: table('users').select('*').where()
+   */
+
+app.listen(3333);
