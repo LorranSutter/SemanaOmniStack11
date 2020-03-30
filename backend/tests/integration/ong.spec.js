7e25e85d701c0b3eafc2f86624aec7bf0168a915
Lorran Sutter <sutterlorran@gmail.com>
2020-03-30 01:01:29 -0400

    Complete ongs tests

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -5,17 +5,18 @@ const connection = require('../../src/db/connection');
 
 describe('ONG', () => {
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         await connection.migrate.rollback(); // Clear test database
         await connection.migrate.latest();
     });
 
     afterAll(async () => {
-       await connection.destroy();
+        await connection.migrate.rollback();
+        await connection.destroy();
     });
 
-    it('should be able to create a new ONG', async () => {
-        const res = await request(app)
+    it('should be able to create a new ONG', (done) => {
+        request(app)
             .post('/ongs')
             // .set('Authorization', 'validId') // header param
             .send({
@@ -24,9 +25,51 @@ describe('ONG', () => {
                 whatsapp: "47000000000",
                 city: "Rio do Sul",
                 state: "SC"
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).toHaveProperty('id');
+                expect(res.body.id).toHaveLength(8);
+                done();
             });
+    });
 
-        expect(res.body).toHaveProperty('id');
-        expect(res.body.id).toHaveLength(8);
+    it('should be able to list registered ONGs', async (done) => {
+        const obj = {
+            name: "APAD1",
+            email: "contato@apad.com.br",
+            whatsapp: "47123456789",
+            city: "Beautiful city",
+            state: "AA"
+        };
+        await request(app)
+            .post('/ongs')
+            .send(obj);
+
+        request(app)
+            .get('/ongs')
+            .expect(200)
+            .end((err, res) => {
+
+                if (err) return done(err);
+
+                expect(() => assert(typeof res.body, Array));
+
+                res.body.forEach(item => {
+                    expect(item).toHaveProperty('id');
+                    expect(item).toHaveProperty('name');
+                    expect(item).toHaveProperty('email');
+                    expect(item).toHaveProperty('whatsapp');
+                    expect(item).toHaveProperty('city');
+                    expect(item).toHaveProperty('state');
+                });
+
+                res.body.some(item => {
+                    expect(() => assert(item, obj));
+                })
+
+                done();
+            })
+
     });
 });
